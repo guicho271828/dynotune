38a1b3120ca1f7dc3fcfaaa18539b5b404069f5a
Masataro Asai <guicho2.71828@gmail.com>
2017-10-10 18:30:47 +0900

    return the result, not list

--- src/optimizers/random-search.lisp
+++ src/optimizers/random-search.lisp
@@ -5,23 +5,27 @@
 (defun random-search (max-trials &key (mode :minimize) keep-results)
   (lambda (function generators)
     (let (acc)
-      (values
-       (ecase mode
-         (:minimize
-          (iter (repeat max-trials)
-                (for parameters = (mapcar #'generate generators))
-                (for result = (apply function parameters))
-                (when keep-results
-                  (push (list result parameters) acc))
-                (finding (list result parameters)
-                         minimizing result)))
-         (:maximize
-          (iter (repeat max-trials)
-                (for parameters = (mapcar #'generate generators))
-                (for result = (apply function parameters))
-                (when keep-results
-                  (push (list result parameters) acc))
-                (finding (list result parameters)
-                         minimizing result))))
-       acc))))
+      (let ((best (ecase mode
+                    (:minimize
+                     (iter (repeat max-trials)
+                           (for parameters = (mapcar #'generate generators))
+                           (for result = (apply function parameters))
+                           (when keep-results
+                             (push (list result parameters) acc))
+                           (finding (list result parameters)
+                                    minimizing result)))
+                    (:maximize
+                     (iter (repeat max-trials)
+                           (for parameters = (mapcar #'generate generators))
+                           (for result = (apply function parameters))
+                           (when keep-results
+                             (push (list result parameters) acc))
+                           (finding (list result parameters)
+                                    minimizing result))))))
+        (match best
+          ((list result parameters)
+           (values
+            result
+            parameters
+            acc)))))))
 
--- t/continuous-optimization.lisp
+++ t/continuous-optimization.lisp
@@ -176,23 +176,24 @@
 
 (test dynotune
   (dolist (fn '(rastrigin
-               ackley
-               sphere
-               rosenbrock
-               beale
-               goldstein-price
-               booth
-               bukin-n6
-               matyas
-               levi
-               three-hump-camel
-               easom
-               cross-in-tray
-               eggholder
-               holder-table
-               mccormick
-               schaffer-n2
-               schaffer-n4
-               styblinski-tang))
-    (dolist (optimizer (list (random-search 100)))
-      (finishes (print (tune fn optimizer))))))
+                ackley
+                sphere
+                rosenbrock
+                beale
+                goldstein-price
+                booth
+                bukin-n6
+                matyas
+                levi
+                three-hump-camel
+                easom
+                cross-in-tray
+                eggholder
+                holder-table
+                mccormick
+                schaffer-n2
+                schaffer-n4
+                styblinski-tang))
+    (print fn)
+    (dolist (optimizer (list (random-search 1000000)))
+      (finishes (print (multiple-value-list (tune fn optimizer)))))))
